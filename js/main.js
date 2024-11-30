4074ace8f99e390a831223928f23bca9e2f84dd7
Andrea Niño <167582509+AndreaBedoya@users.noreply.github.com>
2024-11-29 21:11:15 -0500

    se realizo el modo oscuro del portafolio

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-function modo() {
-    const body = document.querySelector ("#body")
-    body.classList.toggle ("noche")
-}
 
-
-const btnModo =document.querySelector ("#btn-modo")
-document.getElementById("btn-modo").addEventListener("click", modo)
