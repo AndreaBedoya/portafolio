f62b5eda5cdedea46aeb0605e1fd6c42b563f5b7
macro <macrolive578@gmail.com>
2024-12-03 09:47:20 -0500

    ultimos detalles del portafolio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+const descargar = document.getElementById ("descargar")
+ descargar.addEventListener("click", () => {
+    const link= document.createElement("a");
+    link.href = "file:///C:/Users/APRENDIZ/Desktop/portafolio/PORTAFOLIO.pdf";
+    link.download = "AndreaNiño";
+    link.target ="_blank";
+    link.click();
+ })
